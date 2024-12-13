ae2fc48b30b4b8729f4c801732341f0db2b112ca
unknown <vishalreddypasula@gmail.com>
2024-12-13 22:28:21 +0530

    click,hover

---
+++ index.js
@@ -0,0 +1,47 @@
+let h1Tag=document.getElementById("code1")
+console.log(h1Tag)
+h1Tag.style.backgroundColor="orange"
+h1Tag.style.padding="20px"
+h1Tag.style.borderRadius="10px"
+h1Tag.style.cursor="pointer"
+h1Tag.style.boxShadow="10px 10px skyblue"
+
+function demand1(){
+    h1Tag.innerText=" fullStack Institute "
+    h1Tag.style.backgroundColor="orange"
+    h1Tag.style.padding="40px"
+
+}
+
+let a=document.getElementById("code2")
+console.log(a)
+a.style.backgroundColor="brown"
+a.style.padding="40px"
+a.style.borderRadius="10px"
+a.style.boxShadow="10px 10px black"
+
+function demand2(){
+    a.innerText="Flipkart"
+    b.style.color="white"
+    a.style.backgroundColor="blue"
+}
+
+
+let b=document.getElementById("code3")
+b.style.backgroundColor="blue"
+b.style.color="white"
+b.style.padding="20px"
+b.style.borderRadius="10px"
+b.style.cursor="pointer"
+b.style.boxShadow="10px 10px skyblue"
+
+function demand3(){
+    b.innerText="phonepay"
+    b.style.backgroundColor="purple"
+    b.style.color="white"
+    b.style.padding="40px"
+    b.style.Color="white"
+
+
+}
+
